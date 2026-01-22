0b1575e47444dfcf5bfb616c43f3b155ba7db1a6
Rishab Sidhu <rishab.sidhu@intercom.io>
2026-01-22 12:20:11 +0000

    Refactor imports in Login.tsx

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,4 +1,5 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState } from 'react';
+import type { FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import users from './users.json';
 import './Login.css';
